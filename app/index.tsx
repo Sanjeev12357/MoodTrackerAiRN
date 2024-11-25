1215f368b329c500598f6162e26f264b23d2e7b3
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-11-25 23:21:31 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const Home=()=>{
+    return <SafeAreaView className="bg-blue-100">
+        <Text className="text-black"> Home</Text>
+    </SafeAreaView>
+}
+
+export default Home;
